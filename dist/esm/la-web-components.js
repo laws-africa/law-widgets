6e0a56fddf7606a7e507905e2379483c6e710788
longhotsummer <longhotsummer@users.noreply.github.com>
2021-10-20 14:54:54 +0000

    Add built artefacts

---
+++ dist/esm/la-web-components.js
@@ -0,0 +1,17 @@
+import { p as promiseResolve, b as bootstrapLazy } from './index-20c5c9d5.js';
+
+/*
+ Stencil Client Patch Browser v2.9.0 | MIT Licensed | https://stenciljs.com
+ */
+const patchBrowser = () => {
+    const importMeta = import.meta.url;
+    const opts = {};
+    if (importMeta !== '') {
+        opts.resourcesUrl = new URL('.', importMeta).href;
+    }
+    return promiseResolve(opts);
+};
+
+patchBrowser().then(options => {
+  return bootstrapLazy([["la-akoma-ntoso_3",[[4,"la-akoma-ntoso"],[0,"la-decorate-internal-refs",{"akomaNtoso":[1,"akoma-ntoso"],"popups":[4],"flag":[4]}],[0,"la-decorate-terms",{"akomaNtoso":[1,"akoma-ntoso"],"popupDefinitions":[4,"popup-definitions"],"linkTerms":[4,"link-terms"]}]]]], options);
+});
