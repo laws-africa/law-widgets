9d7d8055036e1c0911495a659acadae8525b55e7
Musango <musangow@gmail.com>
2021-10-22 14:27:12 +0200

    table-of-contents.tsx: create public methods expandAll and collaspeAll
    methods can be used to expand and toggle all tocItems

--- src/components/table-of-contents/table-of-contents.tsx
+++ src/components/table-of-contents/table-of-contents.tsx
@@ -1,26 +1,43 @@
-import { Component, Prop, h, Listen } from '@stencil/core';
-import sampleData from './sample-toc.json';
+import { Component, Prop, h, Listen, Element, Method } from '@stencil/core';
 
 @Component({
   tag: 'la-table-of-contents',
 })
 export class TableOfContents {
-  // @ts-ignore
-  @Prop() items: [] = sampleData;
-  @Prop() itemsRenderFromFilter: [] = []
+  @Prop() items: any[] = [];
+  @Prop() itemsFromFilter: any[] = []
+  @Element() el!: HTMLElement;
 
-  @Listen('titleClick')
+  @Listen('titleClicked')
   titleClick(event: CustomEvent) {
     console.log(event.detail)
   }
+
+  @Method()
+  async expandAll() {
+    customElements.whenDefined('la-toc-item')
+      .then(() => {
+        Promise.all([...this.el.querySelectorAll('la-toc-item')]
+          .map(node => node.expand()))
+      });
+  }
+  @Method()
+  async collapseAll() {
+    customElements.whenDefined('la-toc-item')
+      .then(() => {
+        Promise.all([...this.el.querySelectorAll('la-toc-item')]
+          .map(node => node.collapse()))
+      });
+  }
+
   render() {
     return (
       <div class="la-table-of-content">
         <div class="la-table-of-content__items">
-          {this.items.map(item =>
+          {this.items.map((item) =>
             <la-toc-item
               item={item}
-              items-from-filter={this.itemsRenderFromFilter}
+              itemsFromFilter={this.itemsFromFilter}
             />
           )}
         </div>
