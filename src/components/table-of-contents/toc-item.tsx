f78805a2923185be444a77bfd77fbfd4f3d917be
Musango <musangow@gmail.com>
2021-10-22 14:25:48 +0200

    toc-item.tsx: create public methods expand and collapse
    correct children displays
    and plus minus signs that render with expanded

--- src/components/table-of-contents/toc-item.tsx
+++ src/components/table-of-contents/toc-item.tsx
@@ -1,35 +1,36 @@
-import { Component, Prop, h, State, Event, EventEmitter } from '@stencil/core';
-
-//TODO: Integrate type item
-
-// interface ItemShape {
-//   title?: string,
-//   selected?: boolean,
-//   children?: Array<ItemShape>,
-//   index?: number
-// }
+import { Component, Prop, h, State, Event, EventEmitter, Method } from '@stencil/core';
 
 @Component({
   tag: 'la-toc-item',
 })
 export class TocItem {
   @Prop() item: any = {} ;
-  @Prop() itemsFromFilter: [] = []
+  @Prop() itemsFromFilter: any[] = []
 
   @State() expanded: boolean = false;
   @Event({
-    eventName: 'todoCompleted',
+    eventName: 'titleClicked',
     composed: true,
     cancelable: true,
     bubbles: true,
-  }) todoCompleted: EventEmitter<any> | undefined;
+  }) titleClicked: EventEmitter<any> | undefined;
 
-  todoCompletedHandler() {
+  onTitleClick() {
     // @ts-ignore
-    this.todoCompleted.emit(this.item);
+    this.titleClicked.emit(this.item);
   }
 
-  private toggle = () => this.expanded = !this.expanded;
+  @Method()
+  async expand() {
+    this.expanded = true;
+  }
+
+  @Method()
+  async collapse() {
+    this.expanded = false;
+  }
+
+  toggle = () => this.expanded = !this.expanded;
 
   render() {
     const isParent = this.item.children && this.item.children.length;
@@ -38,7 +39,6 @@ export class TocItem {
       if (!this.itemsFromFilter.length) {
         return true;
       }
-      // @ts-ignore
       return this.itemsFromFilter.length && this.itemsFromFilter.some(item => item.title === this.item.title);
     })()
 
@@ -49,7 +49,7 @@ export class TocItem {
         <div class="toc-item__indented">
           {isParent ?
             <button type="button" onClick={this.toggle}>
-              {this.expanded ? "+" : "-"}
+              {this.expanded ? "-" : "+"}
             </button> : null}
         </div>
 
@@ -57,17 +57,18 @@ export class TocItem {
           <div class="toc-item__content__action">
             <button type="button"
                     class="toc-item__content__action__btn"
-                    onClick={() => this.todoCompletedHandler()}
+                    onClick={() => this.onTitleClick()}
             >
               {this.item.title}
             </button>
             <div class="right-icon"></div>
           </div>
           <div class="toc-item__content__children" style={{
-            display: this.expanded ? 'none' : 'block'
+            display: this.expanded ? 'block' : 'none'
           }}>
             {this.item.children && this.item.children.length ?
-              this.item.children.map((item: Object) => <la-toc-item item={item} />)
+              this.item.children
+                .map((item: Object) => <la-toc-item item={item} itemsFromFilter={this.itemsFromFilter}/>)
               : null}
           </div>
         </div>
