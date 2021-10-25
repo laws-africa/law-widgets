39c8811f1d7301af661761f91a97d3bde55e3a98
Musango <musangow@gmail.com>
2021-10-25 10:44:26 +0200

    table-of-controller.tsx: move title query logic to toc component
    move titleQuery logic watcher logic and itemsFromFilter
    make expand and collapse all buttons use bind

--- src/components/table-of-contents-controller/table-of-contents-controller.tsx
+++ src/components/table-of-contents-controller/table-of-contents-controller.tsx
@@ -1,97 +1,42 @@
-import { Component, Prop, h, State, Watch, Element } from '@stencil/core';
+import { Component, Prop, h, State, Element } from '@stencil/core';
 import sampleData from "./sample-toc.json";
 
-
-//TODO: Incorporate types PROPERLY!!!
-type ItemShape =  {
-  title: string,
-  selected: boolean,
-  children?: [],
-  index: number,
-} & { [key: string]: unknown }
+export interface TOCTreeNode {
+  [key: string]: any // type for unknown keys.
+  children?: TOCTreeNode[] // type for a known property.
+}
 
 
 @Component({
   tag: 'la-table-of-contents-controller',
 })
 export class TableOfContentsController {
-  @Prop() items: any[] = sampleData;
+  @Prop() items: TOCTreeNode[] = sampleData;
   @State() titleQuery: string = "";
-  @State() itemsFromFilter: ItemShape[] = [];
   @Element() el!: HTMLElement;
 
-  @Watch('titleQuery')
-  watchStateHandler(newTitleQuery: string) {
-    if (newTitleQuery) {
-      /***
-       * A recursive function that copies the tree, but only retaining children that have a deeper match
-       * When a node matches, no deeper recursion is needed, as then the whole subtree below that node remains
-       * included.
-       * The map will map nodes to false when there is no match somewhere in the subtree rooted by that node.
-       * These false values are then eliminated by filter(Boolean)
-       * */
+  handleTitleChange (e: Event) { this.titleQuery = (e.target as HTMLInputElement).value;}
 
-        // @ts-ignore
-      const filterTree = (nodes, cb) => {
-        return nodes.map((node: ItemShape) => {
-          if (cb(node)) return node;
-          const children = filterTree(node.children || [], cb);
-          return children.length && { ...node, children };
-        }).filter(Boolean);
-      };
-
-      const filteredItems = [...filterTree(this.items, (node: ItemShape) =>
-        node.title.toLowerCase().includes(newTitleQuery.toLowerCase()))];
-
-      const flattenItems = this.flattenItems(filteredItems);
-
-      this.itemsFromFilter = [...flattenItems];
-    } else {
-      this.itemsFromFilter = [];
-    }
-    this.expandAll();
+  async expandAll () {
+    const tocElement = this.el.querySelector('la-table-of-contents');
+    tocElement?.expandAll();
   }
 
-  flattenItems = (items: Array<ItemShape>) => {
-    const flattenItems:Array<ItemShape> = [];
-    const iterateFn = (item:ItemShape) => {
-      flattenItems.push(item);
-      if (item.children) item.children.forEach(iterateFn);
-    };
-
-    items.forEach(iterateFn);
-    return flattenItems;
+  async collapseAll () {
+    const tocElement = this.el.querySelector('la-table-of-contents');
+    tocElement?.collapseAll();
   }
-
-  handleTitleChange = (e: Event) => { this.titleQuery = (e.target as HTMLInputElement).value;}
-
-  expandAll = () => (async () => {
-    await customElements.whenDefined('la-table-of-contents');
-    const tocElement = this.el.querySelector('la-table-of-contents');
-    // @ts-ignore
-    await tocElement.expandAll();
-  })()
-
-  collapseAll = () => (async () => {
-    await customElements.whenDefined('la-table-of-contents');
-    const tocElement = this.el.querySelector('la-table-of-contents');
-    // @ts-ignore
-    await tocElement.collapseAll();
-  })()
 
   render() {
     return (
       <div class="table-of-contents-controller">
-        <input class="title-query-filter" onChange={this.handleTitleChange}/>
+        <input class="title-query-filter" onChange={this.handleTitleChange.bind(this)}/>
         <div class="table-of-contents-controller__toggle">
-          <button type="button" onClick={this.expandAll}>Expand All</button>
-          <button type="button" onClick={this.collapseAll}>Collapse All</button>
+          <button type="button" onClick={this.expandAll.bind(this)}>Expand All</button>
+          <button type="button" onClick={this.collapseAll.bind(this)}>Collapse All</button>
         </div>
         <div class="table-of-contents-controller__toc">
-          <la-table-of-contents
-            items={this.items}
-            itemsFromFilter={this.itemsFromFilter}
-          />
+          <la-table-of-contents items={this.items} titleQuery={this.titleQuery}/>
         </div>
       </div>
     );
