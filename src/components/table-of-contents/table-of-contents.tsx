b4aebbb18c573fe02d642c8cb9976f251dffb400
Musango <musangow@gmail.com>
2021-10-25 11:01:03 +0200

    table-of-contents.tsx: add title search filter logic, refactor methods
    add logic to watcher titleQuery then on change update state itemsFromFilter which is used for showing filtered tocItems

--- src/components/table-of-contents/table-of-contents.tsx
+++ src/components/table-of-contents/table-of-contents.tsx
@@ -1,33 +1,82 @@
-import { Component, Prop, h, Listen, Element, Method } from '@stencil/core';
+import { Prop, h, Listen, Element, Method, Watch, State, Component, Event, EventEmitter } from '@stencil/core';
+import { TOCTreeNode } from '../table-of-contents-controller/table-of-contents-controller';
 
 @Component({
   tag: 'la-table-of-contents',
 })
+
 export class TableOfContents {
-  @Prop() items: any[] = [];
-  @Prop() itemsFromFilter: any[] = []
+  @Prop() items: TOCTreeNode[] = [];
+  @Prop() titleQuery: string = "";
+  @State() itemsFromFilter: TOCTreeNode[] = [];
+
   @Element() el!: HTMLElement;
 
-  @Listen('titleClicked')
-  titleClick(event: CustomEvent) {
-    console.log(event.detail)
-  }
+  @Event({
+    eventName: 'title-clicked',
+    composed: true,
+    cancelable: true,
+    bubbles: true,
+  }) titleClicked: EventEmitter<TOCTreeNode> | undefined;
+
+
+  @Listen('title-clicked-bubble')
+  handleTitleClicked(event: CustomEvent) { this.titleClicked?.emit(event) }
 
   @Method()
   async expandAll() {
-    customElements.whenDefined('la-toc-item')
-      .then(() => {
-        Promise.all([...this.el.querySelectorAll('la-toc-item')]
-          .map(node => node.expand()))
-      });
+    for (const item of this.el.querySelectorAll('la-toc-item')) {
+      item.expand();
+    }
   }
   @Method()
   async collapseAll() {
-    customElements.whenDefined('la-toc-item')
-      .then(() => {
-        Promise.all([...this.el.querySelectorAll('la-toc-item')]
-          .map(node => node.collapse()))
-      });
+    for (const item of this.el.querySelectorAll('la-toc-item')) {
+      item.collapse();
+    }
+  }
+
+  flattenItems = (items: TOCTreeNode[]) => {
+    const flattenItems:TOCTreeNode[] = [];
+    const iterateFn = (item:TOCTreeNode) => {
+      flattenItems.push(item);
+      if (item.children) item.children.forEach(iterateFn);
+    };
+
+    items.forEach(iterateFn);
+    return flattenItems;
+  }
+
+  @Watch('titleQuery')
+  watchStateHandler(newTitleQuery: string) {
+    if (newTitleQuery) {
+      /***
+       * A recursive function that copies the tree, but only retaining children that have a deeper match
+       * When a node matches, no deeper recursion is needed, as then the whole subtree below that node remains
+       * included.
+       * The map will map nodes to false when there is no match somewhere in the subtree rooted by that node.
+       * These false values are then eliminated by filter(Boolean)
+       * */
+
+        // @ts-ignore
+      const filterTree = (nodes, cb) => {
+          return nodes.map((node: TOCTreeNode) => {
+            if (cb(node)) return node;
+            const children = filterTree(node.children || [], cb);
+            return children.length && { ...node, children };
+          }).filter(Boolean);
+        };
+
+      const filteredItems = [...filterTree(this.items, (node: TOCTreeNode) =>
+        node.title.toLowerCase().includes(newTitleQuery.toLowerCase()))];
+
+      const flattenItems = this.flattenItems(filteredItems);
+
+      this.itemsFromFilter = [...flattenItems];
+    } else {
+      this.itemsFromFilter = [];
+    }
+    this.expandAll();
   }
 
   render() {
