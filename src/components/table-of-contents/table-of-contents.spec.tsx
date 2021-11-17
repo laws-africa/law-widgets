9b10d2843885bf6906f547ebf8da976494a0e9ac
Musango <musangow@gmail.com>
2021-11-17 08:58:43 +0200

    update tests for table-of-contents.spec.tsx: began testing method use

--- src/components/table-of-contents/table-of-contents.spec.tsx
+++ src/components/table-of-contents/table-of-contents.spec.tsx
@@ -1,7 +1,11 @@
 import { TableOfContents } from './table-of-contents';
 import { newSpecPage } from '@stencil/core/testing';
 
-const items  = [
+interface ExtraAttributes {
+  [key: string]: any;
+}
+
+const sampleItems  = [
   {
     id: "item-one",
     title: "Item One",
@@ -23,11 +27,26 @@ const createPage = async () => {
   });
 };
 
-const createComponent = (page: any) => {
+const createComponent = (page: any, extraAttributes: ExtraAttributes = {} ) => {
   let component = page.doc.createElement('la-akoma-ntoso');
-  component.items = items;
+  component.items = sampleItems;
+  Object.keys(extraAttributes).forEach(attr => {
+    component[attr] = extraAttributes[attr];
+  });
   return component;
 };
+
+const dataMatchsDOMNodes = (renderedNodes: [], data: Array<{
+  id: string,
+  title: string,
+}>) => {
+  return renderedNodes
+    .every((renderedNode: { querySelector: (arg0: string) => any; title: any; id: any }, index: number) => {
+      const linkElement = renderedNode.querySelector('.content__action__title');
+      return linkElement.innerText === data[index].title && linkElement.href === data[index].id;
+    })
+};
+
 
 describe('la-table-of-contents', () => {
   it('builds', () => {
@@ -38,11 +57,51 @@ describe('la-table-of-contents', () => {
     const page = await createPage();
     const component = createComponent(page);
     const renderedNodes = component.querySelectorAll('la-toc-item');
-    const rendersCorrectly = renderedNodes
-      .every((renderedNode: { querySelector: (arg0: string) => any; title: any; id: any }, index: number) => {
-        const linkElement = renderedNode.querySelector('.content__action__title');
-        return linkElement.innerText === items[index].title && linkElement.href === items[index].id;
+    expect(dataMatchsDOMNodes(renderedNodes, sampleItems)).toBe(true);
+  });
+
+  it('render correct items html based on given data passed in items prop and titleFilter prop', async () => {
+    const page = await createPage();
+    const component = createComponent(page, { titleFilter: "Item Two" });
+    const renderedNodes = component.querySelectorAll('la-toc-item');
+    expect(dataMatchsDOMNodes(renderedNodes, [
+      {
+        id: "item-two",
+        title: "Item Two"
+      },
+    ])).toBe(true);
+  });
+
+  describe('expandAll and collapseAll methods', () => {
+    it('to have been called ', async () => {
+      const page = await createPage();
+      const component = createComponent(page);
+      component.expandAll = jest.fn();
+      component.collapseAll = jest.fn();
+      await component.expandAll();
+      expect(component.expandAll).toHaveBeenCalled();
+      await component.collapseAll();
+      expect(component.collapseAll).toHaveBeenCalled();
     });
-    expect(rendersCorrectly).toBe(true);
+
+    it('expand and collapse rendered items correctly', async () => {
+      const page = await createPage();
+      let component = createComponent(page);
+      page.root?.appendChild(component);
+      await page.waitForChanges();
+
+      console.log(page.root?.querySelector("la-table-of-contents")?.expandAll);
+
+      // const nodeHasExpandedAttribute = (node: { hasAttribute: (arg0: string) => any }) =>
+      //   node.hasAttribute('expanded')
+      //
+      // expect(page.rootInstance.querySelectorAll('la-toc-item').every(nodeHasExpandedAttribute))
+      //   .toBe(true);
+
+      // await component.collapseAll();
+      // expect(component.querySelectorAll('la-toc-item').every(nodeHasExpandedAttribute))
+      //   .toBe(false);
+    });
+
   })
 })
