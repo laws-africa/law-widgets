b45bfb3e33b927ce931f8b4833b053386e55b945
Musango <musangow@gmail.com>
2021-11-19 09:05:18 +0200

    toc-cotroller.spec.tsx:  add tests for various props when set

--- src/components/table-of-contents-controller/table-of-contents-controller.spec.tsx
+++ src/components/table-of-contents-controller/table-of-contents-controller.spec.tsx
@@ -4,20 +4,31 @@ import { TableOfContents } from '../table-of-contents/table-of-contents';
 import { TocItem } from '../toc-item/toc-item';
 import { h } from '@stencil/core';
 
-const sampleItems  = [
+const sampleItems = [
   {
-    id: "item-one",
-    title: "Item One",
+    id: 'item-one',
+    title: 'Item One',
   },
   {
-    id: "item-two",
-    title: "Item Two"
+    id: 'item-two',
+    title: 'Item Two',
   },
   {
-    id: "item-three",
-    title: "Item Three"
-  }
-]
+    id: 'item-three',
+    title: 'Item Three',
+  },
+];
+
+export interface Props {
+  [key: string]: any; // type for unknown keys.
+}
+
+const createPage = async (props: Props = {}) => {
+  return await newSpecPage({
+    components: [TableOfContentsController, TableOfContents, TocItem],
+    template: () => <la-table-of-contents-controller {...props}></la-table-of-contents-controller>,
+  });
+}
 
 describe('la-table-of-contents', () => {
   it('builds', () => {
@@ -25,15 +36,12 @@ describe('la-table-of-contents', () => {
   });
   describe('search input', () => {
     it('renders', async () => {
-      const page = await newSpecPage({
-        components: [TableOfContentsController, TableOfContents, TocItem],
-        template: () => (<la-table-of-contents-controller items={sampleItems}></la-table-of-contents-controller>)
-      });
+      const page = await createPage();
       expect(page.root?.querySelector('.search__input')).toBeTruthy();
     });
     it('input changed', async () => {
       const page = await newSpecPage({
-        components: [TableOfContentsController, TableOfContents, TocItem],
+        components: [TableOfContentsController],
         template: () => (<la-table-of-contents-controller items={sampleItems}></la-table-of-contents-controller>)
       });
       await page.waitForChanges();
@@ -44,8 +52,39 @@ describe('la-table-of-contents', () => {
         input.dispatchEvent(new Event('input' ));
       }
       await page.waitForChanges();
-      console.log(page.root?.outerHTML)
-      expect(jest.fn()).toHaveBeenCalled();
-    })
+      // expect(jest.fn()).toHaveBeenCalled();
+    });
   })
+
+  it('custom title filter placeholder is set', async () => {
+    const placeholder = "Custom placeholder text";
+    const page = await createPage({ titleFilterPlaceholder: placeholder });
+    await page.waitForChanges();
+    const input: HTMLInputElement | undefined | null = page.root?.querySelector('.search__input');
+    expect(input && input.placeholder).toBe(placeholder)
+  })
+
+  it('custom css classes applied to expand all button with expandAllBtnClasses prop set', async () => {
+    const cssClass = "custom-button-class"
+    const page = await createPage({ expandAllBtnClasses: cssClass })
+    await page.waitForChanges();
+    const expandAllBtn: HTMLButtonElement | undefined | null = page.root?.querySelector('.toggle__expand-all-btn');
+    expect(expandAllBtn && expandAllBtn.classList.contains(cssClass)).toBe(true);
+  })
+
+  it('custom css classes applied to collapse all button with collapseAllBtnClasses prop set', async () => {
+    const cssClass = "custom-button-class"
+    const page = await createPage({ collapseAllBtnClasses: cssClass })
+    await page.waitForChanges();
+    const collapseAllBtn: HTMLButtonElement | undefined | null = page.root?.querySelector('.toggle__collapse-all-btn');
+    expect(collapseAllBtn && collapseAllBtn.classList.contains(cssClass)).toBe(true);
+  });
+
+  it('custom css classes applied to title filter search with searchFilterInputClasses prop set', async () => {
+    const cssClass = "custom-button-class"
+    const page = await createPage({ searchFilterInputClasses: cssClass })
+    await page.waitForChanges();
+    const input: HTMLInputElement | undefined | null = page.root?.querySelector('.search__input');
+    expect(input && input.classList.contains(cssClass)).toBe(true);
+  });
 });
