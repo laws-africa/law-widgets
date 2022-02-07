8c8b00b7368f287508037ab998d589f621fb0ea0
Musango <musangow@gmail.com>
2022-02-07 16:55:04 +0200

    gutter.ts: change methods to activateItem and remove scrollIntoView
    add event for layoutComplete

--- src/components/gutter/gutter.ts
+++ src/components/gutter/gutter.ts
@@ -1,4 +1,4 @@
-import { Component, Element, Prop, Listen, Method } from '@stencil/core';
+import { Component, Element, Prop, Listen, Method, Event, EventEmitter } from '@stencil/core';
 import { getAkomaNtosoElement } from '../../utils/linking';
 import { GutterLayout } from './layout';
 import debounce from 'lodash/debounce';
@@ -79,6 +79,15 @@ export class Gutter {
   }
 
   /**
+   * Event emitted when `this.layout` has finished.
+   */
+  @Event({
+    eventName: 'layoutComplete',
+    composed: true,
+    cancelable: true
+  }) layoutComplete!: EventEmitter<void>;
+
+  /**
    * Ensure all items except this one are set as inactive.
    */
   setOtherItemsInactive (activeItem: HTMLLaGutterItemElement) {
@@ -110,49 +119,64 @@ export class Gutter {
   async layoutItems () {
     if (this.layout) {
       this.layout.layout([...this.items()]);
+      this.layoutComplete.emit();
     }
   }
 
+  /**
+   * Method that activates the next item of current active item. The next item's prop `active` will be true. Returns promise with
+   * next active item. If there is no item active,the first item is active. If the current active item is
+   * the last item in the gutter, then when activateNextItem is called, the last item will be active
+   */
   @Method()
-  async navigateToNextItem () {
-    const items = [...this.items()];
-    const activeItemIndex = items.findIndex(item => item.active);
-    items.forEach(item => { item.active = false; });
+  async activateNextItem () {
+    if (this.layout) {
+      const items = this.layout.getSortedItems([...this.items()]);
+      const activeItemIndex = items.findIndex(item => item.active);
+      items.forEach(item => { item.active = false; });
 
-    const nextActiveItem = (() => {
-      // if no gutter item is active or if last gutter item is active then go to first gutter item
-      if (activeItemIndex === -1 || activeItemIndex === items.length - 1) {
-        return items[0];
-      } else {
-        // Otherwise go to next gutter item
-        return items[activeItemIndex + 1];
-      }
-    })();
+      const nextActiveItem = (() => {
+        // if no gutter item is active or if last gutter item is active then go to first gutter item
+        if (activeItemIndex === -1 || activeItemIndex === items.length - 1) {
+          return items[0];
+        } else {
+          // Otherwise go to next gutter item
+          return items[activeItemIndex + 1];
+        }
+      })();
 
-    nextActiveItem.active = true;
-    nextActiveItem.scrollIntoView({ behavior: 'smooth' });
+      nextActiveItem.active = true;
+      return nextActiveItem;
+    }
   }
 
+  /**
+   * Method that activates the previous item of current active item. The next item's prop `active` will be true. Returns promise with
+   * next active item. If there is no item active,the first item is active. If the current active item is
+   * the first item in the gutter, then when activateNextItem is called, the last item will be active
+   */
   @Method()
-  async navigateToPrevItem () {
-    const items = [...this.items()];
-    const activeItemIndex = items.findIndex(item => item.active);
-    items.forEach(item => { item.active = false; });
+  async activatePrevItem () {
+    if (this.layout) {
+      const items = this.layout.getSortedItems([...this.items()]);
+      const activeItemIndex = items.findIndex(item => item.active);
+      items.forEach(item => { item.active = false; });
 
-    const nextActiveItem = (() => {
-      switch (true) {
-        // if no gutter item is active then go to first gutter item
-        case activeItemIndex === -1:
-          return items[0];
-        case activeItemIndex === 0:
-          // if first gutter item is active next go to last gutter item
-          return items[items.length - 1];
-        default:
-          return items[activeItemIndex - 1];
-      }
-    })();
-    nextActiveItem.active = true;
-    nextActiveItem.scrollIntoView({ behavior: 'smooth' });
+      const nextActiveItem = (() => {
+        switch (true) {
+          // if no gutter item is active then go to first gutter item
+          case activeItemIndex === -1:
+            return items[0];
+          case activeItemIndex === 0:
+            // if first gutter item is active next go to last gutter item
+            return items[items.length - 1];
+          default:
+            return items[activeItemIndex - 1];
+        }
+      })();
+      nextActiveItem.active = true;
+      return nextActiveItem;
+    }
   }
 
   items (): NodeListOf<HTMLLaGutterItemElement> {
