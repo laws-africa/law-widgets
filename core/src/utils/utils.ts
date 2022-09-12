4bb69783a661a39024151826e4b56b4b3c6ab126
Musango <musangow@gmail.com>
2022-09-12 17:05:03 +0200

    add argTypeForComponent to util

--- core/src/utils/utils.ts
+++ core/src/utils/utils.ts
@@ -1,6 +1,28 @@
-export function format (first: string, middle: string, last: string): string {
-  return (first || '') + (middle ? ` ${middle}` : '') + (last ? ` ${last}` : '');
+export function argTypesForComponent (component: any) {
+  const argTypes = { ...component.properties };
+
+  for (const val of Object.values(argTypes)) {
+    try {
+      // @ts-ignore
+      val.defaultValue = eval(val.defaultValue);
+    } catch {
+      // @ts-ignore
+    }
+    // @ts-ignore
+    val.description = val.docs.text;
+    // @ts-ignore
+    val.type = {
+      // @ts-ignore
+      name: val.type,
+      // @ts-ignore
+      required: val.required
+    };
+  }
+
+  return argTypes;
 }
+
+
 
 /**
  * Rename element to a different HTML element, copying attributes and children.
